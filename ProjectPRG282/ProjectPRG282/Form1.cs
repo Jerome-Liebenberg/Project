6df6ac4bd88456c52729fbb098c0a8590ae7215c
danielshapovalov <dshapovalov199974@gmail.com>
2019-05-07 21:20:08 +0200

    Form for user logging now using the User class from the Business Layer which will search through the list of all users from the database for Username and Password and if correct, they will proceed to the Communications form.

--- ProjectPRG282/ProjectPRG282/Form1.cs
+++ ProjectPRG282/ProjectPRG282/Form1.cs
@@ -6,11 +6,15 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using Bussiness__Layer;
 
 namespace ProjectPRG282
 {
     public partial class Form1 : Form
-    {
+    { 
+        List<User> l_User = new List<User>();
+        Communications communications = new Communications();
+
         public Form1()
         {
             InitializeComponent();
@@ -31,6 +35,11 @@ namespace ProjectPRG282
             Communications communications = new Communications();
             communications.Show();
             this.Hide();
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
         }
     }
 }
