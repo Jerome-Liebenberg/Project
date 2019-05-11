00eeece8530d395cd1d2612e83646b277c0314f8
danielshapovalov <dshapovalov199974@gmail.com>
2019-05-11 11:39:31 +0200

    Revert "Form for user logging now using the User class from the Business Layer which will search through the list of all users from the database for Username and Password and if correct, they will proceed to the Communications form."

--- ProjectPRG282/Bussiness  Layer/User.cs
+++ ProjectPRG282/Bussiness  Layer/User.cs
@@ -5,7 +5,7 @@ using System.Text;
 
 namespace Bussiness__Layer
 {
-    class User
+    public class User
     {
         private int idNumber;
         private string name;
@@ -23,6 +23,18 @@ namespace Bussiness__Layer
         public int Rank { get => rank; set => rank = value; }
         public string Username { get => username; set => username = value; }
         public string Password { get => password; set => password = value; }
-        
+
+        //NB! Do not show user ID EVER!
+        public User(string _Username, string _Password, string _Name, string _Surname, int _Age)
+        {
+            //this.userID = ; // Need to get UserID without them entering it (Load from Database?)
+            this.username = _Username;
+            this.password = _Password;
+            //this.ranking = ; // Ranking according to the user logged in- get from database?
+            this.name = _Name;
+            this.surname = _Surname;
+            this.age = _Age;
+
+        }
     }
 }
--- ProjectPRG282/ProjectPRG282/Form1.cs
+++ ProjectPRG282/ProjectPRG282/Form1.cs
@@ -6,11 +6,15 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using Bussiness__Layer;
 
 namespace ProjectPRG282
 {
     public partial class Form1 : Form
-    {
+    { 
+        List<User> l_User = new List<User>();
+        Communications communications = new Communications();
+
         public Form1()
         {
             InitializeComponent();
@@ -18,7 +22,7 @@ namespace ProjectPRG282
 
         private void Form1_Load(object sender, EventArgs e)
         {
-
+           
         }
 
         private void button2_Click(object sender, EventArgs e)
@@ -28,9 +32,21 @@ namespace ProjectPRG282
 
         private void button1_Click(object sender, EventArgs e)
         {
-            Communications communications = new Communications();
-            communications.Show();
-            this.Hide();
+            string Username = txtUsername.Text;
+            string Pw = txtPassword.Text;
+            
+            foreach (User user in l_User)  // Need to populate l_User with form load
+            {
+                if (Username == user.Username && Pw == user.Password) // Need to find out if High-Ranking or low-ranking to display appropriate form
+                {
+                    this.Hide();
+                    communications.Show();
+                }
+                else
+                {
+                    MessageBox.Show("Invalid Log in details.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
         }
     }
 }
