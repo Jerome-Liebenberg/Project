fe81f06375d50d78fe34a0c1c48909f3a8d28ab4
danielshapovalov <dshapovalov199974@gmail.com>
2019-05-12 19:25:38 +0200

    Changes

---
+++ ProjectPRG282/Data Access Layer/DB_Access.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.SqlClient;
+using System.Data;
+using System.Windows.Forms;
+
+namespace Data_Access_Layer
+{
+    class DB_Access
+    {
+        SqlConnectionStringBuilder connection = new SqlConnectionStringBuilder();
+        public DB_Access()
+        {
+            // Need to get Server name from datbase for the code below!
+            connection.DataSource = ;
+            connection.InitialCatalog = ;
+            connection.IntegratedSecurity = ;
+        }
+
+        public DataSet ReadData(string _tblName)   // Reading data from the database. Can be used for user login for user ID and User ranking (Adjust code to return these values)
+        {
+            DataSet rawData = new DataSet();
+            SqlConnection conn = new SqlConnection(connection.ToString());
+            string qry = string.Format("SELECT * FROM {0}", _tblName);
+
+            try
+            {
+                conn.Open();
+                SqlDataAdapter adapter = new SqlDataAdapter(qry, conn);
+                adapter.FillSchema(rawData, SchemaType.Source, _tblName);
+                adapter.Fill(rawData,_tblName);
+            }
+            catch (SqlException se)
+            {
+                MessageBox.Show(string.Format("Error : {0}", se.Message));
+            }
+            finally
+            {
+                if (conn.State == ConnectionState.Open)
+                {
+                    conn.Close();
+                }
+            }
+            return rawData;
+        }
+
+        public void InsertDecrptMsg(List<string> _DecryptMsg)  // Insert decrypted message to database // using this method to prevent SQL injection (More secure)
+        {
+            using (SqlConnection conn = new SqlConnection(connection.ToString()))
+            {
+                try
+                {
+                    conn.Open();
+
+                    using (SqlCommand command = new SqlCommand("UpdateDB_DecryptMsg", conn))   // Not sure if the first paramater is correct?
+                    {
+                        command.Parameters.AddWithValue("@DecryptedMessage", _DecryptMsg);       // Mirror the datafield (column) of the database's name in place of the "@DecryptedMessage" eg) Instead of "@DecryptedMessage", in the database it is "@DecryptMsg" so you need to change that to "@DecryptMsg" to match database's field for the decypted messages
+                        command.ExecuteNonQuery();
+                        MessageBox.Show("Message was successfully decrypted and stored.");
+                    }
+                }
+                catch (SqlException se)
+                {
+                    MessageBox.Show(se.Message);
+                }
+                finally
+                {
+                    if (conn.State == ConnectionState.Open)
+                    {
+                        conn.Close();
+                    }
+                }
+                
+            }
+        }
+    }
+}
---
+++ ProjectPRG282/Data Access Layer/File Handler.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Windows.Forms;
+
+namespace Data_Access_Layer
+{
+    class File_Handler
+    {
+        FileStream _FStream;
+        StreamReader _myReader;
+        StreamWriter _myWriter;
+
+        public File_Handler()  // No need to have a Filepath property because we will be reading from and writing to different txtFiles
+        {
+
+        }
+
+        public List<string> EncryptMsg(string _FilePath)   // Method for reading the Encrypted mesage from the txtFile
+        {
+            string line = string.Empty;
+            List<string> EncrptMsg = new List<string>();
+
+            try
+            {
+                _FStream = new FileStream(_FilePath, FileMode.Open, FileAccess.Read);
+                _myReader = new StreamReader(_FStream);
+                line = _myReader.ReadLine();
+
+                while (line != null)                     // Might need to change according to format of txtFile by what seperates each word etc. (I have a photo)
+                {
+                    EncrptMsg.Add(line);
+                    line = _myReader.ReadLine();
+                }
+                // Returns list after the 'Finally'
+            }
+            catch (FileNotFoundException)
+            {
+                MessageBox.Show("File {0} was not found.", _FilePath);
+            }
+            catch (DirectoryNotFoundException)
+            {
+                MessageBox.Show("Directory {0} was not found.", _FilePath);
+            }
+            catch (IOException ioe)
+            {
+                MessageBox.Show("Error: {0} ", ioe.Message);
+            }
+            finally
+            {
+                _myReader.Close();
+                _FStream.Close();
+            }
+            return EncrptMsg;
+        }
+
+        public List<string> DecryptMsg(string _FilePath) // Method for writing the Unencrypted text to the Database (Don't know how to write to DB with txtFile)
+        {
+            string line = string.Empty;
+            List<string> DecrptMsg = new List<string>();
+
+            try
+            {
+                _FStream = new FileStream(_FilePath, FileMode.Truncate, FileAccess.Write);  // Use Truncate becasue every encrypted message will be different therefore existing txtFile must be overwritten
+                _myWriter = new StreamWriter(_FStream);
+                line = _myReader.ReadLine();
+
+                //while (line != null)                                        // INSERT DECRYPTION CODE HERE!!!!
+                //{
+                //    DecrptMsg.Add(line);
+                //    line = _myReader.ReadLine();
+                //}
+
+                // Returns list after the 'Finally'
+            }
+            catch (FileNotFoundException)
+            {
+                MessageBox.Show("File {0} was not found.", _FilePath);
+            }
+            catch (DirectoryNotFoundException)
+            {
+                MessageBox.Show("Directory {0} was not found.", _FilePath);
+            }
+            catch (IOException ioe)
+            {
+                MessageBox.Show("Error: {0} ", ioe.Message);
+            }
+            finally
+            {
+                _myReader.Close();
+                _FStream.Close();
+            }
+            return DecrptMsg;
+        }
+
+    }
+}
